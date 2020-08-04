260aa3f8d8cf2e8668992adae9bd0b545409001e
ihsanberahim <ihsanberahim@gmail.com>
2020-08-04 11:47:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-dpzfhj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-dpzfhj)
---
+++ index.ts
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `
+<div class="cross">
+<img src="https://lh3.googleusercontent.com/proxy/SkENuvE76unrvZiDxfnYbHAsoTfpbCqlMaExBDNrrCEj0bygCf0TYg8faUYW0F5z1amkrB1Cq2Lk_39JqeXfxt2pKSgBe-f8rSxQ-jHcJ_NMTXNdbGtuurmAarQBWUSfa-MH_7nIfCo">
+</div>
+`;
